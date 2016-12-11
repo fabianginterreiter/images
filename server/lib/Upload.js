b182975c3943b916223004c9891ce312d0e154ea
Fabian Ginterreiter <fabian.ginterreiter@gmail.com>
2016-12-11 11:55:35 +0100

    Move helper method to separate files.

--- server/lib/Upload.js
+++ server/lib/Upload.js
@@ -1,66 +1,10 @@
-var fs = require('fs-extra');
-
-var sizeOf = require('image-size');
-
-var sharp = require('sharp');
-
-var ExifImage = require('exif').ExifImage;
-
 var Image = require('../model/Image');
 
 var config = require('../config');
 
 var ImageInfo = require('./ImageInfo');
-
-function copy(file, image) {
-  var directory = image.year + '/' + image.month;
-
-  image.path = directory + '/' + image.filename;
-
-  return new Promise(function(resolve, reject) {
-    fs.ensureDir(config.getImagesPath() + '/' + directory, function(err) {
-      if (err) {
-        return reject(err);
-      }
-
-      fs.copy(file, config.getImagesPath() + '/' + image.path, function(err) {
-        if (err) {
-          return reject(err);
-        }
-
-        fs.unlink(file, function(err) {
-          if (err) {
-            return reject(err);
-          }
-
-          resolve(image);
-        });
-      });
-    });
-  });
-};
-
-function thumb(image, target, width, height) {
-  var directory = image.year + '/' + image.month;
-
-  return new Promise(function(resolve, reject) {
-    fs.ensureDir(target + '/' + directory, function(err) {
-      if (err) {
-        return reject(err);
-      }
-
-      sharp(config.getImagesPath() + '/' + image.path)
-        .resize(width, height)
-        .max().toFile(target + '/' + image.path, function(err) {
-          if (err) {
-            return reject(err);
-          }
-
-          resolve(image);
-        });
-    });
-  });
-}
+var CopyImageFile = require('./CopyImageFile');
+var ResizeImage = require('./ResizeImage');
 
 module.exports = function(file) {
   return new Promise(function(resolve, reject) {
@@ -78,11 +22,11 @@ module.exports = function(file) {
       result.month = result.date.getMonth() + 1;
       result.day = result.date.getDate();
 
-      return copy(file.path, result);
+      return CopyImageFile(file.path, result);
     }).then(function(result) {
-      return thumb(result, config.getThumbnailPath(), 400, 200);
+      return ResizeImage(result, config.getThumbnailPath(), 400, 200);
     }).then(function(result) {
-      return thumb(result, config.getPreviewPath(), 2000, 2000);
+      return ResizeImage(result, config.getPreviewPath(), 2000, 2000);
     }).then(function(result) {
       return new Image(result).save();
     }).then((result) => (resolve(result.toJSON()))).catch(reject);

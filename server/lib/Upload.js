fd3514127e5fa8bc14c7a07820747217a8300ff6
Fabian Ginterreiter <fabian.ginterreiter@gmail.com>
2016-12-11 11:44:54 +0100

    Use Promises for Uploading Image.

--- server/lib/Upload.js
+++ server/lib/Upload.js
@@ -10,95 +10,81 @@ var Image = require('../model/Image');
 
 var config = require('../config');
 
-module.exports = function(file) {
-  return new Promise(function(resolve, reject) {
-    var result = {
-      filename: file.originalname, 
-      size: file.size
-    };
+var ImageInfo = require('./ImageInfo');
 
-    sizeOf(file.path, function (err, dimensions) {
+function copy(file, image) {
+  var directory = image.year + '/' + image.month;
+
+  image.path = directory + '/' + image.filename;
+
+  return new Promise(function(resolve, reject) {
+    fs.ensureDir(config.getImagesPath() + '/' + directory, function(err) {
       if (err) {
-        console.log(err);
         return reject(err);
       }
 
-      result.width = dimensions.width;
-      result.height = dimensions.height;
-
-      new ExifImage({ image : file.path }, function (err, exifData) {
+      fs.copy(file, config.getImagesPath() + '/' + image.path, function(err) {
         if (err) {
-          console.log(err);
           return reject(err);
         }
 
-        result.date = new Date(exifData.exif.CreateDate.replace(':', '.').replace(':', '.'));
+        fs.unlink(file, function(err) {
+          if (err) {
+            return reject(err);
+          }
 
-        result.year = result.date.getFullYear();
-        result.month = result.date.getMonth() + 1;
-        result.day = result.date.getDate();
-
-        new Image(result).save().then(function(data) {
-
-
-          fs.ensureDir(config.getImagesPath() + '/' + result.year + '/' + result.month, function(err) {
-            if (err) {
-              return reject(err);
-            }
-
-            var subpath = result.year + '/' + result.month + '/' + data.get('id') + '.jpg';
-
-            fs.copy(file.path, config.getImagesPath() + '/' + subpath, function(err) {
-              if (err) {
-                return reject(err);
-              }
-
-              data.set('path', subpath);
-
-              data.save().then(function(data) {
-                
-                fs.ensureDir(config.getThumbnailPath() + '/' + result.year + '/' + result.month, function(err) {
-                  if (err) {
-                    return reject(err);
-                  }
-
-                  sharp(config.getImagesPath() + '/' + subpath)
-                  .resize(400, 200)
-                  .max().toFile(config.getThumbnailPath() + '/' + subpath, function(err) {
-                    if (err) {
-                      return reject(err);
-                    }
-
-                    fs.ensureDir(config.getPreviewPath() + '/' + result.year + '/' + result.month, function(err) {
-                      if (err) {
-                        return reject(err);
-                      }
-
-                       sharp(config.getImagesPath() + '/' + subpath)
-                      .resize(2000, 2000)
-                      .max().toFile(config.getPreviewPath() + '/' + subpath, function(err) {
-                        if (err) {
-                          return reject(err);
-                        }
-
-                        fs.unlink(file.path, function(err) {
-                          if (err) {
-                            return reject(err);
-                          }
-
-                          resolve(data.toJSON());
-                        });
-                      });
-                    });
-                  });
-                });
-              }).catch(function(err) {
-                reject(err);
-              });
-            });
-          });
+          resolve(image);
         });
       });
     });
+  });
+};
+
+function thumb(image, target, width, height) {
+  var directory = image.year + '/' + image.month;
+
+  return new Promise(function(resolve, reject) {
+    fs.ensureDir(target + '/' + directory, function(err) {
+      if (err) {
+        return reject(err);
+      }
+
+      sharp(config.getImagesPath() + '/' + image.path)
+        .resize(width, height)
+        .max().toFile(target + '/' + image.path, function(err) {
+          if (err) {
+            return reject(err);
+          }
+
+          resolve(image);
+        });
+    });
+  });
+}
+
+module.exports = function(file) {
+  return new Promise(function(resolve, reject) {
+    ImageInfo(file.path).then(function(info) {
+      var result = {
+        filename: file.originalname, 
+        size: file.size
+      };
+
+      result.width = info.width;
+      result.height = info.height;
+      result.date = info.date;
+
+      result.year = result.date.getFullYear();
+      result.month = result.date.getMonth() + 1;
+      result.day = result.date.getDate();
+
+      return copy(file.path, result);
+    }).then(function(result) {
+      return thumb(result, config.getThumbnailPath(), 400, 200);
+    }).then(function(result) {
+      return thumb(result, config.getPreviewPath(), 2000, 2000);
+    }).then(function(result) {
+      return new Image(result).save();
+    }).then((result) => (resolve(result.toJSON()))).catch(reject);
   });
 };

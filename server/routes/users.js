71a23873b09bfba9637de242bb3303b02d2a53bc
Fabian Ginterreiter <fabian.ginterreiter@gmail.com>
2016-12-13 23:20:38 +0100

    Fix route for images and add route for users.

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,34 +1,32 @@
 const express = require('express');
 const passport = require('passport');
-const ImagesController = require('../controllers/ImagesController');
+const UsersController = require('../controllers/UsersController');
 const router = express.Router();
 
 const multer  = require('multer')
 const upload = multer({ dest: 'uploads/' })
 
 router.get('/', (req, res) => {
-  new ImagesController(req).index().then(function(images) {
-    res.send(images);
-  }).catch(function(e) {
+  new UsersController(req).index().then((users) => (res.send(users))).catch(function(e) {
     res.status(404).send('Fehler');
   });
 });
 
 router.get('/:id', (req, res) => {
-  new ImagesController(req).get().then((image) => (res.send(image))).catch(function(e) {
+  new UsersController(req).get().then((user) => (res.send(user))).catch(function(e) {
     res.status(404).send('Fehler');
   });
 });
 
 router.delete('/:id', (req, res) => {
-  new ImagesController(req).destroy().then((image) => (res.send(image))).catch(function(e) {
+  new UsersController(req).destroy().then((user) => (res.send(user))).catch(function(e) {
     res.status(404).send('Fehler');
   });
 });
 
-router.post('/', upload.single('image'), function(req, res) {
-  new ImagesController(req).create().then(function(result) {
-    res.send(result);
+router.post('/', function(req, res) {
+  new UsersController(req).create().then(function(user) {
+    res.send(user);
   }).catch(function(err) {
     res.send(err);
   })

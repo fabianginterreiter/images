a9af4582ef6e3f20791fd11e99220bd2b62c1d53
Fabian Ginterreiter <fabian.ginterreiter@gmail.com>
2017-01-09 21:14:29 +0100

    Edit tags in tag view.

--- client/Tags.js
+++ client/Tags.js
@@ -1,10 +1,12 @@
 "use strict"
 
 import React from 'react'
-
 import Images from './components/Images'
 import ImagesStore from './stores/ImagesStore'
 import ImagesNav from './components/ImagesNav'
+import{ Quickedit, DialogStore } from './utils/Utils'
+import Ajax from './libs/Ajax'
+import { browserHistory } from 'react-router'
 
 class Tags extends React.Component {
 
@@ -32,13 +34,48 @@ class Tags extends React.Component {
     });
   }
 
+  handleChange(value) {
+    if (this.state.tag.name === value) {
+      this.setState({
+        edit: false
+      })
+    } else {
+      this.state.tag.name = value;
+      Ajax.put('/api/tags/' + this.state.tag.id, this.state.tag).then((tag) => {
+        this.setState({
+          tag:tag,
+          edit:false
+        });
+      });
+    }
+  }
+
+  renderTitle() {
+    if (this.state.edit) {
+      return (<Quickedit 
+        value={this.state.tag.name} 
+        onChange={this.handleChange.bind(this)} 
+        onCancel={() => this.setState({edit:false})} />);
+    }
+
+    return this.state.tag.name;
+  }
+
+  handleDelete() {
+    DialogStore.open('Delete Tag', 'Do you really want to delete the Tags?')
+    .then((result) => Ajax.delete('/api/tags/' + this.state.tag.id)).then(() => {
+      browserHistory.push('/images');
+    }).catch((e) => console.log(e));
+  }
+
   render() {
     return (
       <div>
         <h1>
-          <i className="fa fa-tag" aria-hidden="true" /> {this.state.tag.name}
+          <i className="fa fa-tag" aria-hidden="true" /> {this.renderTitle()}
           <ImagesNav>
-            <button onClick={this.handleEdit.bind(this)} className="primary">Edit</button>
+            <button onClick={this.handleEdit.bind(this)} className="primary"><i className="fa fa-pencil-square-o" /><span className="min500"> Edit</span></button>
+            <button onClick={this.handleDelete.bind(this)} className="warning"><i className="fa fa-trash-o" /><span className="min500"> Delete</span></button>
           </ImagesNav>
         </h1>
         <Images />

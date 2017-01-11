b38df44410a2ae94d49cccb9c5c5a598a4969015
Fabian Ginterreiter <fabian.ginterreiter@gmail.com>
2017-01-11 23:24:30 +0100

    Create query for tags and persons.

--- server/controllers/SearchController.js
+++ server/controllers/SearchController.js
@@ -9,24 +9,36 @@ class SearchController extends BaseController {
   index() {
     let words = this.query.s.split(" ");
 
-    return ImagePerson.query((qb) => {
-      qb.select('images_persons.image_id');
-      qb.count('images_persons.image_id AS count');
-      qb.join('persons', 'persons.id', 'images_persons.person_id')
-
-      qb.where('persons.name', 'like', '%' + words[0] + '%');
-
-      for (var index = 1; index < words.length; index++) {
-        qb.orWhere('persons.name', 'like', '%' + words[index] + '%');
-      }
-
-      qb.groupBy('images_persons.image_id');
-      qb.orderBy('count', 'desc');
-      qb.debug(true);
-    }).fetchAll().then((result) => result.toJSON());
+    return bookshelf.knex
+      .select('image_id', 'images.*')
+      .count('image_id AS count')
+      .from(function() {
+        this.select('image_id')
+          .from('images_persons')
+          .join('persons', 'persons.id', 'images_persons.person_id')
+          .where(function() {
+            let w = this.where('persons.name', 'like', '%' + words[0] + '%');
+            for (var index = 1; index < words.length; index++) {
+              w.orWhere('persons.name', 'like', '%' + words[index] + '%');
+            }
+          })
+          .unionAll(function() {
+            this.select('image_id')
+              .from('images_tags')
+              .join('tags', 'tags.id', 'images_tags.tag_id')
+              .where(function() {
+                let w = this.where('tags.name', 'like', '%' + words[0] + '%');
+                for (var index = 1; index < words.length; index++) {
+                  w.orWhere('tags.name', 'like', '%' + words[index] + '%');
+                }
+              })
+          })//todo: album
+      })
+      .join('images', 'images.id', 'image_id')
+      .groupBy('image_id')
+      .orderBy('count', 'desc')
+      .then((result) => result);
   }
-
- 
 }
 
 module.exports = SearchController;
